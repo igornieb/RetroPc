1cf7c9735cd1c194c0c8feded1ba125fc75cdbdb
igorniebylski <igor.niebylski@gmail.com>
2023-11-14 14:28:50 +0100

    fixed types, added baisic bus

--- Bus.kt
+++ Bus.kt
@@ -1,36 +1,50 @@
-class Bus constructor(private var ram : ByteArray = ByteArray(256*256), private var cpu : Cpu6502 = Cpu6502()){
+class Bus {
+    @OptIn(ExperimentalUnsignedTypes::class)
+    private var ram : UByteArray = UByteArray(256*256)
+    private var cpu : Cpu6502 = Cpu6502()
     init {
         // reset memory
-        for(i in (0..ram.size))
+        for(i in (0..ram.size-1))
         {
-            ram[i]=0x00;
+            ram[i]=0u
         }
 
         //connect cpu with bus
-        cpu.ConnectBus(this);
+        cpu.ConnectBus(this)
     }
 
-    public fun Read(address:Short) : Byte
+    fun Read(address:UInt) : UByte
     {
         // currently ram is only thing on the bus so reading is done to range - 0-256^2
-        if (address in 0x0000..0xffff)
-        {
-            return ram[address.toInt()];
+        if (address in (0x0000u..0xffffu)) {
+            return ram[address.toInt()]
         }
-
-        return 0x00;
+        return 0.toUByte();
     }
 
-    public fun Write(address: Int, data: Int){
+    fun Write(address : UInt, data : UByte){
         // currently ram is only thing on the bus so writing is done to its range - 0-256^2
-        if (address in 0x0000..0xffff)
+        if (address.toInt() in 0x0000..0xffff)
         {
             ram[address.toInt()] = data
         }
     }
 
-    public fun LoadInstructions(address:Short, data:String)
+    fun LoadInstructions(address:UInt, data:String)
     {
-        // TODO load instructions
+        // setting starting point to address
+        // after Reset method is called on cpu, this location will be read and execution will start from address
+        ram[0xFFFC] = address.toUByte()
+
+        var hex : List<String> = data.split(" ")
+        var tmpAddress = address
+        for (element in hex)
+        {
+            var value = element.toUByte(radix = 16)
+            Write(tmpAddress, value)
+            tmpAddress++
+        }
+        println("Program loaded");
+
     }
 }
