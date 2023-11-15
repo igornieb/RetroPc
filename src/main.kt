898590f002aa635c2e8c79ad08ea46ba94422b8e
igorniebylski <igor.niebylski@gmail.com>
2023-11-15 16:43:52 +0100

    minor changes

--- src/main.kt
+++ src/main.kt
@@ -10,20 +10,19 @@ fun main()
     // ORA #11 -> sets A register (12) to A|11 -> 15
     // LDX #12 -> set X register to 12
     // LDY #10 -> set Y register to 10
-    // those instructions were converted to hex values and loaded to Ram on the bus from given starting address
-    var hex : String = "A9 0c 09 0b a0 0a a2 0c"
-    bus.LoadInstructions(0x00F0u, hex)
+    var hex = "A9 0c 09 0b a0 0a a2 0c"
+    bus.LoadInstructions(0x00FFu, hex)
 
     var cpu = Cpu6502()
     cpu.ConnectBus(bus)
     cpu.Reset()
 
     // executing instructions
-    var ins = 0;
-    while (ins<16)
+    var ins = 0
+    while (ins < 16)
     {
-        cpu.Info()
         cpu.Clock()
         ins += 1
+        cpu.Info()
     }
 }
