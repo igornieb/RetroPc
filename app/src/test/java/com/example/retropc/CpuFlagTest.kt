953bd5217b3344bdcb35f85512df0b2b42fe4fea
Filip <filipgolyszny@gmail.com>
2024-01-22 16:41:55 +0100

    Implement SEC, CLC, SED, CLD, SEI, CLI and CLV tests

--- app/src/test/java/com/example/retropc/CpuFlagTest.kt
+++ app/src/test/java/com/example/retropc/CpuFlagTest.kt
@@ -10,6 +10,8 @@ class CpuFlagTest {
     @Before
     fun setup() {
         cpu = Cpu6502()
+        var bus = Bus()
+        cpu.ConnectBus(bus)
     }
 
     @Test
@@ -92,4 +94,168 @@ class CpuFlagTest {
         // Assert
         assertEquals(flag.value.inv().toUByte(), cpu.Sr())
     }
+    @Test
+    fun testSEC() {
+        //Assembly
+        /* SEC  */
+
+        // Instructions
+        /* Set Carry Flag */
+
+        // Arrange
+        val initialSr = 0b0000_0000.toUByte()
+        cpu.SetFlags(initialSr)
+        val hex = "38"
+        val instruction_length = 1
+
+
+        // Act
+        cpu.runInstruction(hex, instruction_length)
+
+        // Assert
+        assertEquals(1, cpu.GetFlag(Flags.C))
+    }
+
+    @Test
+    fun testCLC() {
+        //Assembly
+        /* SEC
+        CLC  */
+
+        // Instructions
+        /* Set Carry Flag
+        * Clear Carry Flag */
+
+        // Arrange
+        val initialSr = 0b0000_0000.toUByte()
+        cpu.SetFlags(initialSr)
+        val hex = "38 18"
+        val instruction_length = 2
+
+
+        // Act
+        cpu.runInstruction(hex, instruction_length)
+
+        // Assert
+        assertEquals(0, cpu.GetFlag(Flags.C))
+    }
+
+    @Test
+    fun testSED() {
+        //Assembly
+        /* SED  */
+
+        // Instructions
+        /* Set Decimal Flag */
+
+        // Arrange
+        val initialSr = 0b0000_0000.toUByte()
+        cpu.SetFlags(initialSr)
+        val hex = "f8"
+        val instruction_length = 1
+
+
+        // Act
+        cpu.runInstruction(hex, instruction_length)
+
+        // Assert
+        assertEquals(1, cpu.GetFlag(Flags.D))
+    }
+
+    @Test
+    fun testCLD() {
+        //Assembly
+        /* SED
+        CLD  */
+
+        // Instructions
+        /* Set Decimal Flag
+        * Clear Decimal Flag */
+
+        // Arrange
+        val initialSr = 0b0000_0000.toUByte()
+        cpu.SetFlags(initialSr)
+        val hex = "f8 d8"
+        val instruction_length = 2
+
+
+        // Act
+        cpu.runInstruction(hex, instruction_length)
+
+        // Assert
+        assertEquals(0, cpu.GetFlag(Flags.D))
+    }
+
+    @Test
+    fun testSEI() {
+        //Assembly
+        /* SEI  */
+
+        // Instructions
+        /* Set interrupt Flag */
+
+        // Arrange
+        val initialSr = 0b0000_0000.toUByte()
+        cpu.SetFlags(initialSr)
+        val hex = "78"
+        val instruction_length = 1
+
+
+        // Act
+        cpu.runInstruction(hex, instruction_length)
+
+        // Assert
+        assertEquals(1, cpu.GetFlag(Flags.I))
+    }
+
+    @Test
+    fun testCLI() {
+        //Assembly
+        /* SEI
+        CLI  */
+
+        // Instructions
+        /* Set interrupt Flag
+        * Clear interrupt Flag */
+
+        // Arrange
+        val initialSr = 0b0000_0000.toUByte()
+        cpu.SetFlags(initialSr)
+        val hex = "78 58"
+        val instruction_length = 2
+
+
+        // Act
+        cpu.runInstruction(hex, instruction_length)
+
+        // Assert
+        assertEquals(0, cpu.GetFlag(Flags.I))
+    }
+
+    @Test
+    fun testCLV() {
+        //Assembly
+        /* LDA #$7F
+        ADC #$01
+        CLV  */
+
+        // Instructions
+        /* Load 0x7f to accumulator
+        * Add 1 to accumulator to set overflow flag
+        * Clear overflow Flag */
+
+        // Arrange
+        val initialSr = 0b0000_0000.toUByte()
+        cpu.SetFlags(initialSr)
+        val hex = "a9 7f 69 01 b8"
+        val instruction_length = 3
+
+
+        // Act
+        cpu.runInstruction(hex, instruction_length)
+
+        // Assert
+        assertEquals(0, cpu.GetFlag(Flags.V))
+    }
+
 }
